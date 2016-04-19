e1396de26b584fadb9b25732c9bb089229dfb422
Scott Leberknight <scottleber@gmail.com>
2016-04-18 23:22:57 -0400

    Add migrations bundle; database support with JDBI; support for env. var substitution in configuration.

--- devignition-service/src/main/java/com/devignition/service/DevIgnitionApplication.java
+++ devignition-service/src/main/java/com/devignition/service/DevIgnitionApplication.java
@@ -1,10 +1,19 @@
 package com.devignition.service;
 
 import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.db.PooledDataSourceFactory;
+import io.dropwizard.jdbi.DBIFactory;
+import io.dropwizard.jdbi.bundles.DBIExceptionsBundle;
+import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.skife.jdbi.v2.DBI;
 
 public class DevIgnitionApplication extends Application<DevIgnitionConfiguration> {
+
+    private static final boolean STRICT_ENVIRONMENT_VARS = true;
 
     public static void main(final String[] args) throws Exception {
         new DevIgnitionApplication().run(args);
@@ -12,18 +21,39 @@ public class DevIgnitionApplication extends Application<DevIgnitionConfiguration
 
     @Override
     public String getName() {
-        return "Devignition Service";
+        return "DevIgnition Service";
     }
 
     @Override
     public void initialize(final Bootstrap<DevIgnitionConfiguration> bootstrap) {
-        // TODO: application initialization
+        enableEnvironmentVariableSubstitution(bootstrap);
+        addMigrationsBundle(bootstrap);
+        bootstrap.addBundle(new DBIExceptionsBundle());
     }
 
     @Override
-    public void run(final DevIgnitionConfiguration configuration,
-                    final Environment environment) {
+    public void run(final DevIgnitionConfiguration configuration, final Environment environment) {
+
+        DBIFactory jdbiFactory = new DBIFactory();
+        DBI jdbi = jdbiFactory.build(environment, configuration.getDataSourceFactory(), "database");
+
         // TODO: implement application
+    }
+
+    private void enableEnvironmentVariableSubstitution(Bootstrap<DevIgnitionConfiguration> bootstrap) {
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(STRICT_ENVIRONMENT_VARS))
+        );
+    }
+
+    private void addMigrationsBundle(Bootstrap<DevIgnitionConfiguration> bootstrap) {
+        bootstrap.addBundle(new MigrationsBundle<DevIgnitionConfiguration>() {
+            @Override
+            public PooledDataSourceFactory getDataSourceFactory(DevIgnitionConfiguration config) {
+                return config.getDataSourceFactory();
+            }
+        });
     }
 
 }
--- devignition-service/src/main/java/com/devignition/service/DevIgnitionConfiguration.java
+++ devignition-service/src/main/java/com/devignition/service/DevIgnitionConfiguration.java
@@ -1,7 +1,23 @@
 package com.devignition.service;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@ToString
 public class DevIgnitionConfiguration extends Configuration {
-    // TODO: implement service configuration
+
+    @JsonProperty("database")
+    @Valid
+    @NotNull
+    private DataSourceFactory dataSourceFactory;
+
 }
