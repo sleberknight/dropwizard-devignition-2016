43a9ec65e967f792e91c39b07be18191a5f5c689
Scott Leberknight <scottleber@gmail.com>
2016-04-22 16:51:39 -0400

    Setup env var substitution, database migrations bundle, and Hibernate bundle in Nest app. Also add deps for Spring-based testing.

--- nest-service/src/main/java/com/devignition/nest/NestApplication.java
+++ nest-service/src/main/java/com/devignition/nest/NestApplication.java
@@ -1,10 +1,21 @@
 package com.devignition.nest;
 
 import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.db.PooledDataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
+import io.dropwizard.hibernate.ScanningHibernateBundle;
+import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class NestApplication extends Application<NestConfiguration> {
+
+    // Setting to false lets us define default values in case an environment var isn't present
+    private static final boolean STRICT_ENVIRONMENT_VARS = false;
+
+    private HibernateBundle<NestConfiguration> hibernateBundle;
 
     public static void main(final String[] args) throws Exception {
         new NestApplication().run(args);
@@ -17,13 +28,43 @@ public class NestApplication extends Application<NestConfiguration> {
 
     @Override
     public void initialize(final Bootstrap<NestConfiguration> bootstrap) {
-        // TODO: application initialization
+        enableEnvironmentVariableSubstitution(bootstrap);
+        addMigrationsBundle(bootstrap);
+
+        String entityPackage = "com.devignition.nest";
+        hibernateBundle = newHibernateBundle(entityPackage);
+        bootstrap.addBundle(hibernateBundle);
     }
 
     @Override
     public void run(final NestConfiguration configuration,
                     final Environment environment) {
         // TODO: implement application
+    }
+
+    private void enableEnvironmentVariableSubstitution(Bootstrap<NestConfiguration> bootstrap) {
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(STRICT_ENVIRONMENT_VARS))
+        );
+    }
+
+    private void addMigrationsBundle(Bootstrap<NestConfiguration> bootstrap) {
+        bootstrap.addBundle(new MigrationsBundle<NestConfiguration>() {
+            @Override
+            public PooledDataSourceFactory getDataSourceFactory(NestConfiguration config) {
+                return config.getDataSourceFactory();
+            }
+        });
+    }
+
+    private HibernateBundle<NestConfiguration> newHibernateBundle(final String entityPackage) {
+        return new ScanningHibernateBundle<NestConfiguration>(entityPackage) {
+            @Override
+            public PooledDataSourceFactory getDataSourceFactory(NestConfiguration configuration) {
+                return configuration.getDataSourceFactory();
+            }
+        };
     }
 
 }
--- nest-service/src/main/java/com/devignition/nest/NestConfiguration.java
+++ nest-service/src/main/java/com/devignition/nest/NestConfiguration.java
@@ -1,7 +1,23 @@
 package com.devignition.nest;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@ToString
 public class NestConfiguration extends Configuration {
-    // TODO: implement service configuration
+
+    @JsonProperty("database")
+    @Valid
+    @NotNull
+    private DataSourceFactory dataSourceFactory;
+
 }
