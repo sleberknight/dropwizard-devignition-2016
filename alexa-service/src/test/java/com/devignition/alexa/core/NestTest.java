c6c9543b05a8ab1ac45f607ec277a33511ca9f1d
Scott Leberknight <scottleber@gmail.com>
2016-04-23 16:37:39 -0400

    Add test for deserializing Nest instances.

--- alexa-service/src/test/java/com/devignition/alexa/core/NestTest.java
+++ alexa-service/src/test/java/com/devignition/alexa/core/NestTest.java
@@ -2,29 +2,38 @@ package com.devignition.alexa.core;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.jackson.Jackson;
+import io.dropwizard.testing.FixtureHelpers;
 import org.junit.Test;
 
 import java.io.IOException;
 
-import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NestTest {
 
     private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
 
+    private static String NEST_JSON = FixtureHelpers.fixture("fixtures/basementNest.json");
+
     @Test
     public void testJsonSerialization() throws IOException {
+        String expected = MAPPER.writeValueAsString(MAPPER.readValue(NEST_JSON, Nest.class));
 
-        String nestJson = fixture("fixtures/basementNest.json");
-        String expected = MAPPER.writeValueAsString(MAPPER.readValue(nestJson, Nest.class));
+        Nest nest = nestForTest();
+        assertThat(MAPPER.writeValueAsString(nest)).isEqualTo(expected);
+    }
 
-        Nest nest = Nest.builder()
+    @Test
+    public void testJsonDeserialization() throws IOException {
+        assertThat(MAPPER.readValue(NEST_JSON, Nest.class)).isEqualTo(nestForTest());
+    }
+
+    private Nest nestForTest() {
+        return Nest.builder()
                 .id(1L)
                 .location("Basement")
                 .locationId(88L)
                 .build();
-        assertThat(MAPPER.writeValueAsString(nest)).isEqualTo(expected);
     }
 
 }
